d964b266d0385ac78dc502dc1f7940c9921286c9
Soumyadip Adhikary <adhikarysoumyadip811@gmail.com>
2026-05-01 11:01:25 +0530

    fix: express wildcard route crash

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,15 +25,9 @@ app.use(rateLimiter)
 app.use('/api/notes', router)
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static(path.join(__dirname, "../frontend/dist")))
-
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  // app.use((req, res) => {
-  //   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-  // })
-
-  app.get("/*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
   });
 }
